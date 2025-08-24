6694aac82129fdab06d786bd1c0727ed894c8971
Akanksha Vadnekar <arvadnekar@mun.ca>
2025-08-24 00:17:51 -0230

    fix: type errors

--- server/src/type/express.d.ts
+++ server/src/type/express.d.ts
@@ -1,7 +1,17 @@
-import 'express';
+// import 'express';
 
-declare module 'express' {
-  export interface Request {
+// declare module 'express' {
+//   export interface Request {
+//     userId?: string; // userId added by middleware
+//   }
+// }
+
+
+import "express-serve-static-core";
+
+declare module "express-serve-static-core" {
+  interface Request {
     userId?: string; // userId added by middleware
   }
+  // Do NOT redeclare Response unless you are adding custom properties!
 }
