dce746d642af00ccb919bd72107efe1a7c02353f
Akanksha Vadnekar <arvadnekar@mun.ca>
2025-08-24 00:01:16 -0230

    feat: initial page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,8 +4,17 @@ import Ctasection from "@/components/cts";
 import Featuresection from "@/components/featuresection";
 import Herosection from "@/components/hero";
 import {TestimonialsGrid } from "@/components/TestimonialCard";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { isLoaded, isSignedIn, user, } = useUser();
+   if (!isLoaded) {
+    return <div>Loading...</div>;
+   }
+   if (isSignedIn) {
+    redirect('/onboarding')
+   }
   return (
     <>
       <Herosection />
